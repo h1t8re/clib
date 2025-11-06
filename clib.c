619c0a085e2432a2a693603880f86b2ca35a3232
h1t8re <lahbabi1chems3eddine@gmail.com>
2025-11-06 22:10:30 +0100

    Incomplete clib

---
+++ clib.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+const int strlen(const char *str)
+{
+  int length = 0;
+  while(*(str+length) != '\0')
+      length++;
+  return length;
+}
+const char *strdup(const char *str)
+{
+  int str_len = 0;
+  char *str_dup = (char *)malloc(sizeof(char));
+  while( *(str+str_len) != '\0' )
+  {
+    *(str_dup+(str_len*sizeof(char))) = *(str_len*sizeof(char));
+    str_len++;
+    *(str_dup+(str_len*sizeof(char))) = '\0';
