a92807e4c3c168526bc5f37f6eaf748537b9bd2c
h1t8re <lahbabi1chems3eddine@gmail.com>
2025-11-07 10:14:20 +0100

    Update clib.c

--- clib.c
+++ clib.c
@@ -13,11 +13,11 @@ const char *strdup(const char *str)
 	int str_len = 0;
 	char *str_dup = (char *)malloc(sizeof(char));
 	while( *(str+str_len) != '\0' )
-  {
+	{
 		*(str_dup+(str_len*sizeof(char))) = *(str_len*sizeof(char));
 		str_len++;
-		*(str_dup+(str_len*sizeof(char))) = '\0';
 	}
+	*(str_dup+(str_len*sizeof(char))) = '\0';
 	return str_dup;
 }
 
